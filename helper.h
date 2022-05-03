e23b387626092cc1a2245b325f0628e710e361eb
anky163 <nguyenducanky96@gmail.com>
2022-05-04 04:36:05 +0700

    MERGE SORT

---
+++ helper.h
@@ -0,0 +1,266 @@
+#include <stdio.h>
+
+
+int allocate(int length, int side);
+void divide(int array[], int left[], int right[], int length);
+void rearrange(int subs_array[], int left[], int right[], int length);
+void sort(int array[], int length);
+void swap(int *ptr1, int *ptr2);
+
+int turn_to_number(char *word);
+
+
+
+/*
+int main(void)
+{
+    int array[] = {5, -1, 2, 7, 9, 8, 8, -1, 3, 4, 1, 6, 3, 0};
+    int length = 14;
+    sort(array, length);
+    for (int i = 0; i < length; i++)
+    {
+        printf("%i ", array[i]);
+    }
+    printf("\n");
+}
+*/
+
+int allocate(int length, int side)
+{
+    int left_count = 0;
+    int right_count = 0;
+
+    if (length % 2 == 0)
+    {
+        left_count = length / 2;
+    }
+    else
+    {
+        left_count = (length + 1) / 2;
+    }
+    right_count = length - left_count;
+
+    if (side < 0)
+    {
+        return left_count;
+    }
+    else
+    {
+        return right_count;
+    }
+}
+
+
+
+void divide(int array[], int left[], int right[], int length)
+{
+    int left_count = allocate(length, -1);
+    int right_count = allocate(length, 1);
+
+    int half = right_count;
+
+    // 1 5 3 2 4 6
+    // 1 5 3
+    // 2 4 6
+
+    for (int i = 0; i < half; i++)
+    {
+        left[i] = array[i];
+        if (right_count < left_count)
+        {
+            right[i] = array[i + half + 1];
+        }
+        else
+        {
+            right[i] = array[i + half];
+        }
+    }
+    if (right_count < left_count)
+    {
+        left[left_count - 1] = array[left_count - 1];
+    }
+    return;
+}
+
+
+void rearrange(int subs_array[], int left[], int right[], int length)
+{
+    int left_count = allocate(length, -1);
+    int right_count = allocate(length, 1);
+
+    // 2 4 5 7 | 0 1 3 6
+    int cursor1 = 0;
+    int cursor2 = 0;
+
+    for (int i = 0; i < length; i++)
+    {
+        if (cursor1 >= left_count || cursor2 >= right_count)
+        {
+            if (cursor2 >= right_count && cursor1 < left_count)
+            {
+                subs_array[i] = left[cursor1];
+                cursor1++;
+            }
+            else if (cursor1 >= left_count && cursor2 < right_count)
+            {
+                subs_array[i] = right[cursor2];
+                cursor2++;
+            }
+        }
+
+        else
+        {
+            if (left[cursor1] <= right[cursor2])
+            {
+                subs_array[i] = left[cursor1];
+                cursor1++;
+            }
+            else
+            {
+                subs_array[i] = right[cursor2];
+                cursor2++;
+            }
+        }
+    }
+    return;
+}
+
+
+
+void sort(int array[], int length)
+{
+    int left_count = allocate(length, -1);
+    int right_count = allocate(length, 1);
+
+    int left[left_count];
+    int right[right_count];
+
+    // base case
+    if (length == 1)
+    {
+        return;
+    }
+    else if (length == 2)
+    {
+        if (array[0] <= array[1])
+        {
+            return;
+        }
+        else
+        {
+            swap(&array[0], &array[1]);
+            return;
+        }
+    }
+
+    // divide array into 2 halves
+    divide(array, left, right, length);
+
+    // sort each half in order
+    sort(left, left_count);
+    sort(right, right_count);
+
+    // merge those 2 halves
+    rearrange(array, left, right, length);
+    return;
+}
+
+
+void swap(int *ptr1, int *ptr2)
+{
+    int a = *ptr1;
+    int b = *ptr2;
+
+    int *tmp = &a;
+    *ptr1 = b;
+    *ptr2 = *tmp;
+    return;
+}
+
+
+
+int turn_to_number(char *word)
+{
+    int number = 0;
+    for (int i = 0, n = strlen(word); i < n; i++)
+    {
+        if (word[i] != '-')
+        {
+            int m = ((int) word[i] - 48) * pow(10, (n - 1 - i));
+            number += m;
+        }
+    }
+    if (word[0] == '-')
+    {
+        number += -1;
+    }
+    return number;
+}
+
+
+
+
+
+
+
+/*
+int main(void)
+{
+    //int array[] = {2, 4, 5, 7, 8, 10, 0, 1, 3, 6, 9};
+    //int length = 11;
+
+    //int array[] = {2};
+    //int length = 1;
+
+    int array[] = {5, 2, 7, 9, 8, 8, 3, 4, 1, 6, 3, 0};
+    int length = 12;
+
+    int left_count = allocate(length, -1);
+    int right_count = allocate(length, 1);
+
+    //printf("%i %i\n", left_count, right_count);
+
+    int left[left_count];
+    int right[right_count];
+
+
+    divide(array, left, right, length);
+
+    printf("left: ");
+    for (int i = 0; i < left_count; i++)
+    {
+        printf("%i ", left[i]);
+    }
+    printf("\n");
+    printf("right: ");
+    for (int i = 0; i < right_count; i++)
+    {
+        printf("%i ", right[i]);
+    }
+    printf("\n");
+
+    int subs_array[length];
+    rearrange(subs_array, left, right, length);
+
+    printf("subs_array: ");
+    for (int i = 0; i < length; i++)
+    {
+        printf("%i ", subs_array[i]);
+    }
+    printf("\n");
+    printf("left_count: %i\n", left_count);
+    printf("right_count: %i\n", right_count);
+    printf("\n");
+
+    sort(array, length);
+    printf("array: ");
+    for (int i = 0; i < length; i++)
+    {
+        printf("%i ", array[i]);
+    }
+    printf("\n");
+    printf("\n");
+
+    return 0;
+}
+*/
---
+++ sort.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+#include "helper.h"
+
+#define text1 "random5000.txt"
+#define text2 "random10000.txt"
+#define text3 "random50000.txt"
+#define text4 "reversed5000.txt"
+#define text5 "reversed10000.txt"
+#define text6 "reversed50000.txt"
+#define text7 "sorted5000.txt"
+#define text8 "sorted10000.txt"
+#define text9 "sorted50000.txt"
+
+#define TOTAL 9
+
+
+int main(int argc, char *argv[])
+{
+    char *list[] = {text1, text2, text3, text4, text5, text6, text7, text8, text9};
+
+    if (argc != 2)
+    {
+        printf("Usage: ./sort filename\n");
+        return 1;
+    }
+
+    int count = 0;
+    for (int i = 0; i < TOTAL; i++)
+    {
+        if (strcmp(argv[1], list[i]) == 0)
+        {
+            count++;
+        }
+    }
+    if (count != 1)
+    {
+        printf("Could not open file.sort/\n");
+        return 1;
+    }
+
+    char *text = argv[1];
+
+    FILE *open = fopen(text, "r");
+    if (open == NULL)
+    {
+        printf("Cannot open file.\n");
+        return 1;
+    }
+
+    char word[100];
+    count = 0;
+    while (fscanf(open, "%s", word) != EOF)
+    {
+        count++;
+    }
+    fclose(open);
+
+    // determine size of int array
+    int array[count];
+
+    // put every number from file into array
+    open = fopen(text, "r");
+    if (open == NULL)
+    {
+        printf("Cannot open file.\n");
+        return 1;
+    }
+
+    char s[100];
+    int index = 0;
+    while (fscanf(open, "%s", s) != EOF)
+    {
+        int number = turn_to_number(s);
+        array[index] = number;
+        index++;
+    }
+/*
+    for (int i = 0; i < count; i++)
+    {
+        printf("%i\n", array[i]);
+    }
+*/
+    fclose(open);
+
+    const int LENGTH = count;
+    sort(array, LENGTH);
+    for (int i = 0; i < count; i++)
+    {
+        printf("%i\n", array[i]);
+    }
+    return 0;
+}
